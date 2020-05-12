d03da90e2fb60c10048668b1cbd492d33ce691bf
AlexeyAlive <54289587+AlexeyAlive@users.noreply.github.com>
2020-05-12 19:38:58 +0300

    Add files via upload

---
+++ src/main/java/haifa/ac/il/OCSFSimpleChatChat/App.java
@@ -0,0 +1,13 @@
+package haifa.ac.il.OCSFSimpleChatChat;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
